dbdb42008e8e422a2c1fe989a8d172c2e782c22b
minhphuong <minhphuongit1992@gmail.com>
2016-12-23 14:20:25 +0700

    diemdanh

--- aptech/Scripts/DiemDanh.js
+++ aptech/Scripts/DiemDanh.js
@@ -12,7 +12,10 @@
                 html += '<td>' + value.svID + '</td>';
                 html += '<td>' + value.svTen + '</td>';
                 html += '<td>' + value.lopID + '</td>';
-                html += '<td> <input type="checkbox" name="your-group" value="' + value.svID + '" /> </td>';
+                html += '<td> <input type = "radio" checked value="cm" name="' + value.svID + '" /> </td>';
+                html += '<td> <input type = "radio" value="cp"  name="' + value.svID + '" /> </td>';
+                html += '<td> <input type = "radio" value="kp"  name="' + value.svID + '" /> </td>';
+                html += '<td> <input type="text" id="' + value.svID + '" disabled="disabled" /> </td>';
                 html += '</tr>';
             });
             $('#ds tbody').html(html);
@@ -23,3 +26,26 @@
     });
     return false;
 }
+
+$(function () {
+    $("input:checkbox").on('click', function () {
+        // in the handler, 'this' refers to the box clicked on
+        var $box = $(this);
+        if ($box.is(":checked")) {
+            // the name of the box is retrieved using the .attr() method
+            // as it is assumed and expected to be immutable
+            var group = "input:checkbox[name='" + $box.attr("name") + "']";
+            // the checked state of the group/box on the other hand will change
+            // and the current value is retrieved using .prop() method
+            $(group).prop("checked", false);
+            $box.prop("checked", true);
+        } else {
+            $box.prop("checked", false);
+        }
+        if($('input[name="'+ $box.attr("name") +'"]:checked').val() == "cp")
+        {
+            var group1 = "input:text[id='" + $box.attr("id") + "']";
+            document.getElementById(group1).disabled = false;
+        }
+    });
+});
